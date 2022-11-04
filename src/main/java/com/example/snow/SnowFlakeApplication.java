566b866c880d4e72df05ee6bfe071cfe777ae954
wxz <759126349@qq.com>
2022-11-04 23:04:49 +0800

    init

--- src/main/java/com/example/snow/SnowFlakeApplication.java
+++ src/main/java/com/example/snow/SnowFlakeApplication.java
@@ -3,6 +3,11 @@ package com.example.snow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author wxz
+ * @date 20:16 2022/10/22
+ */
 @SpringBootApplication
 public class SnowFlakeApplication {
 
--- src/main/java/com/example/snow/utils/SnowFlakeUtils.java
+++ src/main/java/com/example/snow/utils/SnowFlakeUtils.java
@@ -1,2 +1,77 @@
-package com.example.snow.utils;public class SnowFlakeUtils {
+package com.example.snow.utils;
+
+
+import com.baomidou.mybatisplus.core.toolkit.Sequence;
+
+import java.net.InetAddress;
+
+/**
+ * mybatis plus 实现雪花算法
+ * @author wxz
+ * @date 20:55 2022/10/22
+ */
+public class SnowFlakeUtils {
+
+    /**
+     * 核心实现
+     */
+    private final Sequence sequence;
+
+    /**
+     *
+     * @author wxz
+     * @date 21:05 2022/10/22
+     */
+    public SnowFlakeUtils() {
+        this.sequence = new Sequence(null);
+    }
+
+    /**
+     *
+     * @author wxz
+     * @date 21:05 2022/10/22
+     */
+    public SnowFlakeUtils(InetAddress inetAddress) {
+        this.sequence = new Sequence(inetAddress);
+    }
+
+    /**
+     *
+     * @author wxz
+     * @date 21:05 2022/10/22
+     */
+    public SnowFlakeUtils(long workerId, long dataCenterId) {
+        this.sequence = new Sequence(workerId, dataCenterId);
+    }
+
+    /**
+     *
+     * @author wxz
+     * @date 21:05 2022/10/22
+     */
+    public SnowFlakeUtils(Sequence sequence) {
+        this.sequence = sequence;
+    }
+
+    /**
+     * 生成主键ID 生成61位二进制数字（0和1）然后转成十进制
+     * 长度 = 19的十进制数字
+     * @author wxz
+     * @date 21:02 2022/10/22 
+     * @return java.lang.Long
+     */
+    public Long nextId() {
+        return this.sequence.nextId();
+    }
+
+    /**
+     *
+     * @author wxz
+     * @date 21:04 2022/10/22
+     */
+    public static void main(String[] args) {
+        SnowFlakeUtils snowFlakeUtils = new SnowFlakeUtils();
+        System.out.println(snowFlakeUtils.nextId());
+    }
+
 }
