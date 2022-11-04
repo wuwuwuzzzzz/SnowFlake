cea6425996b5f094adc9653a8f524f704b8a2546
wxz <759126349@qq.com>
2022-11-04 22:38:54 +0800

    Initial commit

---
+++ src/test/java/com/example/snow/SnowFlakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowFlakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
